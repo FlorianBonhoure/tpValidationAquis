9b2fb650aee77aac393860b2aaf4a1a85afbf95d
Florian Bonhoure <florian.b47@hotmail.fr>
2022-11-04 12:38:53 +0100

    add database

---
+++ src/main/etudiants.sql
@@ -0,0 +1,15 @@
+-- create table if not exists etudiant (
+-- 	id varchar(50) not null primary key,
+-- 	nom varchar(50) not null,
+-- 	moyenne float,
+-- 	image varchar(50)
+-- );
+
+-- insert into etudiant (id,nom,moyenne, image) 
+-- values ('1','Etudiant1',12.0,'./src/main/webapp/images/titeuf.jpg'),
+-- ('2','Etudiant2',14.0,'./src/main/webapp/images/cedric.jpg'),
+-- ('3','Etudiant3',9.0,'./src/main/webapp/images/nicolas.jpg'),
+-- ('4','Etudiant4',15.0,'./src/main/webapp/images/frank.jpg'),
+-- ('5','Etudiant5',8.0,'./src/main/webapp/images/ducobu.jpg');
+
+select * from etudiant;
